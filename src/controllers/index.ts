3021d55e806dd74f6f532035739887a0d6bf183b
jay-bhogayata <jaybhogayata53@gmail.com>
2024-04-18 02:10:23 +0530

    refactor: Refactor controllers to use asyncHandler for error handling and add custom error class

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -2,13 +2,18 @@ import { Request, Response } from "express";
 
 import user from "../database/schema/user";
 import { db } from "../database/connection";
+import asyncHandler from "../utils/asyncHandler";
 
-export const handleHealthCheck = async (req: Request, res: Response) => {
-  res.status(200).json({ message: "OK" });
-};
+export const handleHealthCheck = asyncHandler(
+  async (req: Request, res: Response) => {
+    res.status(200).json({ message: "OK" });
+  }
+);
 
-export const testDatabase = async (req: Request, res: Response) => {
-  await db.insert(user).values({ name: "jay" });
-  const u = await db.query.user.findMany();
-  res.status(200).json({ message: u });
-};
+export const testDatabase = asyncHandler(
+  async (req: Request, res: Response) => {
+    await db.insert(user).values({ name: "jay" });
+    const u = await db.query.user.findMany();
+    res.status(200).json({ message: u });
+  }
+);
