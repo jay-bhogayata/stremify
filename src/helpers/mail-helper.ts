11aacc6df91b3aa875adf5590619d0371dface21
jay-bhogayata <jaybhogayata53@gmail.com>
2024-04-19 15:15:12 +0530

    fix: error handling in sendMail function

--- src/helpers/mail-helper.ts
+++ src/helpers/mail-helper.ts
@@ -1,5 +1,6 @@
 import config from "../config/index";
 import transporter from "../config/transporter";
+import CustomError from "../utils/customError";
 
 interface MailOptions {
   to: string;
@@ -9,15 +10,20 @@ interface MailOptions {
 }
 
 const sendMail = async (mailOptions: MailOptions) => {
-  const message = {
-    from: config.AWS_SES_SENDER_EMAIL,
-    to: mailOptions.to,
-    subject: mailOptions.subject,
-    text: mailOptions.text,
-    html: mailOptions.html,
-  };
+  try {
+    const message = {
+      from: config.AWS_SES_SENDER_EMAIL,
+      to: mailOptions.to,
+      subject: mailOptions.subject,
+      text: mailOptions.text,
+      html: mailOptions.html,
+    };
 
-  await transporter.sendMail(message);
+    await transporter.sendMail(message);
+  } catch (error) {
+    console.error("error in sending email", error);
+    throw new CustomError("Failed to send email.", 500);
+  }
 };
 
 export default sendMail;
