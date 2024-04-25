a3be54a1fdfb9678ace507b02efc6c08e4c81b40
jay-bhogayata <jaybhogayata53@gmail.com>
2024-04-25 18:50:17 +0530

    feat: Add Redis as session store implementation

---
+++ src/database/kv.ts
@@ -0,0 +1,22 @@
+import { createClient } from "redis";
+import RedisStore from "connect-redis";
+import { logger } from "../utils/logger";
+
+let redisClient = createClient();
+
+redisClient.connect();
+
+redisClient.on("connect", () => {
+  logger.info("Redis client connected");
+});
+redisClient.on("error", (err) => {
+  logger.error(`Redis client error: ${err}`);
+  process.exit(1);
+});
+
+let sessionStore = new RedisStore({
+  client: redisClient,
+  prefix: "session:",
+});
+
+export default sessionStore;
