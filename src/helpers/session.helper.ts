73a55d4bd987755e5898ccdc1a47c3b5117e4d5f
jay-bhogayata <jaybhogayata53@gmail.com>
2024-04-25 18:51:27 +0530

    feat: Add session helper module for managing session configuration

---
+++ src/helpers/session.helper.ts
@@ -0,0 +1,44 @@
+import session from "express-session";
+import config from "../config";
+import { v4 as uuid } from "uuid";
+import sessionStore from "../database/kv";
+
+// TODO: set secure to true in production , it is false for development, set domain to our domain , set ("trust proxy", 1) in production if we are behind a proxy
+
+export const sessionConfig: session.SessionOptions = {
+  cookie: {
+    path: "/",
+    httpOnly: true,
+    secure: false,
+    maxAge: 24 * 60 * 60 * 1000,
+  },
+  secret: config.SESSION_SECRET,
+  genid: () => {
+    return uuid();
+  },
+  name: "sessionId",
+  resave: false,
+  saveUninitialized: false,
+  store: sessionStore,
+};
+
+export const sessionConfigSecure: session.SessionOptions = {
+  cookie: {
+    path: "/",
+    httpOnly: true,
+    secure: true,
+    maxAge: 24 * 60 * 60 * 1000,
+    sameSite: "lax",
+  },
+  secret: config.SESSION_SECRET,
+  proxy: true,
+  genid: () => {
+    return uuid();
+  },
+  name: "sessionId",
+  resave: false,
+  saveUninitialized: false,
+  store: sessionStore,
+};
+
+export default { sessionConfig, sessionConfigSecure };
