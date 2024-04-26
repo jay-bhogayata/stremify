169d909c5566b51b7c3b790f3562fc8098321033
jay-bhogayata <jaybhogayata53@gmail.com>
2024-04-26 18:59:27 +0530

    test: add protected route endpoint for testing login

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -13,5 +13,8 @@ authRouter.post("/signup", signUp);
 authRouter.post("/verify/:userID", verifyUser);
 authRouter.post("/login", login);
 authRouter.post("/logout", isAuthenticated, logout);
+authRouter.get("/protected", isAuthenticated, (req: Request, res: Response) => {
+  res.json({ message: "This is a protected route" });
+});
 
 export default authRouter;
