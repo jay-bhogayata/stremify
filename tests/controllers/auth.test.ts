fda5c7a13450763d49fe17ec7fcca773ae5249b4
jay-bhogayata <jaybhogayata53@gmail.com>
2024-04-29 21:30:02 +0530

    test: add test for login and logout

--- tests/controllers/auth.test.ts
+++ tests/controllers/auth.test.ts
@@ -1,4 +1,6 @@
 import {
+  login,
+  logout,
   signUp,
   validateUser,
   verifyUser,
@@ -314,5 +316,232 @@ describe("validateUser", () => {
     await expect(validateUser("test@example.com", "password")).rejects.toThrow(
       new CustomError("user is not verified", 401)
     );
+  });
+});
+
+describe("login", () => {
+  it("logs in the user successfully", async () => {
+    const mockReq = {
+      body: {
+        email: "test@example.com",
+        password: "password",
+      },
+      session: {
+        isLoggedIn: false,
+        user: null,
+        regenerate: jest.fn(),
+      },
+    } as unknown as Request;
+
+    const mockRes = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    } as unknown as Response;
+
+    const mockUser = {
+      id: "11f49ff-2cbb-4b66-8b73-53a3bee6103b",
+      name: "Test User",
+      email: "test@example.com",
+      password: "hashedPassword",
+      verified: true,
+      role: "guest" as const,
+    };
+
+    (
+      getUserByEmail as jest.MockedFunction<typeof getUserByEmail>
+    ).mockResolvedValueOnce(mockUser);
+    (
+      comparePassword as jest.MockedFunction<typeof comparePassword>
+    ).mockResolvedValueOnce(true);
+
+    await login(mockReq, mockRes, jest.fn());
+
+    expect(getUserByEmail).toHaveBeenCalledWith(mockReq.body.email, db);
+    expect(comparePassword).toHaveBeenCalledWith(
+      mockReq.body.password,
+      mockUser.password
+    );
+    expect(mockReq.session.isLoggedIn).toBe(true);
+    expect(mockReq.session.regenerate).toHaveBeenCalled();
+    expect(mockRes.status).toHaveBeenCalledWith(200);
+    expect(mockRes.json).toHaveBeenCalledWith({ message: "login successful" });
+  });
+
+  it("returns an error for an unverified user", async () => {
+    const mockReq = {
+      body: {
+        email: "test@example.com",
+        password: "password",
+      },
+    } as unknown as Request;
+
+    const mockRes = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    } as unknown as Response;
+
+    const mockUser = {
+      id: "11f49ff-2cbb-4b66-8b73-53a3bee6103b",
+      name: "Test User",
+      email: "test@example.com",
+      role: "guest" as const,
+      verified: false,
+      password: "hashedPassword",
+    };
+
+    (
+      getUserByEmail as jest.MockedFunction<typeof getUserByEmail>
+    ).mockResolvedValueOnce(mockUser);
+
+    await login(mockReq, mockRes, jest.fn());
+
+    expect(getUserByEmail).toHaveBeenCalledWith(mockReq.body.email, db);
+    expect(comparePassword).not.toHaveBeenCalled();
+    expect(mockRes.status).toHaveBeenCalledWith(401);
+    expect(mockRes.json).toHaveBeenCalledWith({
+      message: "user is not verified",
+    });
+  });
+
+  it("returns an error for an invalid password", async () => {
+    const mockReq = {
+      body: {
+        email: "test@example.com",
+        password: "wrongPassword",
+      },
+    } as unknown as Request;
+
+    const mockRes = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    } as unknown as Response;
+
+    const mockUser = {
+      id: "11f49ff-2cbb-4b66-8b73-53a3bee6103b",
+      name: "Test User",
+      email: "test@example.com",
+      role: "guest" as const,
+      verified: true,
+      password: "hashedPassword",
+    };
+
+    (
+      getUserByEmail as jest.MockedFunction<typeof getUserByEmail>
+    ).mockResolvedValueOnce(mockUser);
+    (
+      comparePassword as jest.MockedFunction<typeof comparePassword>
+    ).mockResolvedValueOnce(false);
+
+    await login(mockReq, mockRes, jest.fn());
+
+    expect(getUserByEmail).toHaveBeenCalledWith(mockReq.body.email, db);
+    expect(comparePassword).toHaveBeenCalledWith(
+      mockReq.body.password,
+      mockUser.password
+    );
+    expect(mockRes.status).toHaveBeenCalledWith(401);
+    expect(mockRes.json).toHaveBeenCalledWith({ message: "Invalid password" });
+  });
+
+  it("returns an error for a non-existent user", async () => {
+    const mockReq = {
+      body: {
+        email: "test@example.com",
+        password: "password",
+      },
+    } as unknown as Request;
+
+    const mockRes = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    } as unknown as Response;
+
+    (
+      getUserByEmail as jest.MockedFunction<typeof getUserByEmail>
+    ).mockResolvedValueOnce(null);
+
+    await login(mockReq, mockRes, jest.fn());
+
+    expect(getUserByEmail).toHaveBeenCalledWith(mockReq.body.email, db);
+    expect(comparePassword).not.toHaveBeenCalled();
+    expect(mockRes.status).toHaveBeenCalledWith(404);
+    expect(mockRes.json).toHaveBeenCalledWith({ message: "user not found" });
+  });
+
+  it("handles unexpected errors", async () => {
+    const mockReq = {
+      body: {
+        email: "test@example.com",
+        password: "password",
+      },
+    } as unknown as Request;
+
+    const mockRes = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    } as unknown as Response;
+
+    const mockError = new CustomError("Internal server error", 500);
+
+    (
+      getUserByEmail as jest.MockedFunction<typeof getUserByEmail>
+    ).mockRejectedValueOnce(mockError);
+
+    await login(mockReq, mockRes, jest.fn());
+
+    expect(getUserByEmail).toHaveBeenCalledWith(mockReq.body.email, db);
+    expect(comparePassword).not.toHaveBeenCalled();
+    expect(mockRes.status).toHaveBeenCalledWith(500);
+    expect(mockRes.json).toHaveBeenCalledWith({
+      message: "Internal server error",
+    });
+  });
+});
+
+describe("logout", () => {
+  it("logs out the user successfully", async () => {
+    const mockReq = {
+      session: {
+        destroy: jest.fn((callback) => callback()),
+      },
+    } as unknown as Request;
+
+    const mockRes = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+      clearCookie: jest.fn(),
+    } as unknown as Response;
+
+    await logout(mockReq, mockRes, jest.fn());
+
+    expect(mockReq.session.destroy).toHaveBeenCalled();
+    expect(mockRes.clearCookie).toHaveBeenCalledWith("sessionId");
+    expect(mockRes.status).toHaveBeenCalledWith(200);
+    expect(mockRes.json).toHaveBeenCalledWith({
+      message: "Logged out successfully",
+    });
+  });
+
+  it("handles an error during logout", async () => {
+    const mockReq = {
+      session: {
+        destroy: jest.fn((callback) => callback(new Error("Logout error"))),
+      },
+    } as unknown as Request;
+
+    const mockRes = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+      clearCookie: jest.fn(),
+    } as unknown as Response;
+
+    await logout(mockReq, mockRes, jest.fn());
+
+    expect(mockReq.session.destroy).toHaveBeenCalled();
+    expect(mockRes.clearCookie).not.toHaveBeenCalled();
+    expect(mockRes.status).toHaveBeenCalledWith(500);
+    expect(mockRes.json).toHaveBeenCalledWith({
+      message: "Could not log out, please try again",
+    });
   });
 });
