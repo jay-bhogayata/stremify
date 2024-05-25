13d56e5a962feeb968298de0c229add53427d48d
jay-bhogayata <jaybhogayata53@gmail.com>
2024-05-25 17:25:08 +0530

    infra: setup rds postgres using terraform

--- infra/main.tf
+++ infra/main.tf
@@ -7,7 +7,7 @@ terraform {
   }
   backend "s3" {
     bucket         = "stremify-tfstate"
-    key            = "state/terraform.tfstate"
+    key            = "stremify-backend/terraform.tfstate"
     region         = "ap-south-1"
     encrypt        = true
     dynamodb_table = "stremify-tfstate-lock"
@@ -19,40 +19,48 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-
-resource "aws_s3_bucket" "stremify_raw_vods" {
-  bucket        = "stremify-raw-vods"
-  force_destroy = true
-  tags = {
-    app : "stremify"
-  }
-}
-
-resource "aws_s3_bucket_cors_configuration" "cors_for_raw_vods" {
-  bucket = aws_s3_bucket.stremify_raw_vods.id
-  cors_rule {
-    allowed_headers = ["*"]
-    allowed_methods = ["PUT", "POST", "GET"]
-    allowed_origins = ["*"]
-    expose_headers  = ["ETag"]
-  }
-
-}
-
-resource "aws_s3_bucket" "stremify_prod_vods" {
-  bucket        = "stremify-prod-vods"
-  force_destroy = true
-  tags = {
-    app : "stremify"
-  }
-}
-
-output "buckets_info" {
-  value = {
-    raw_vods  = aws_s3_bucket.stremify_raw_vods.bucket
-    prod_vods = aws_s3_bucket.stremify_prod_vods.bucket
-  }
+data "aws_vpc" "default" {
+  default = true
 }
 
 
+resource "aws_db_instance" "rdb" {
+  identifier        = "stremify"
+  allocated_storage = 20
+  storage_type      = "gp2"
+  engine            = "postgres"
+  engine_version    = "16.1"
+  instance_class    = "db.t3.micro"
+  db_name           = var.pg_db_name
+  username          = var.pg_db_username
+  password          = var.pg_db_password
 
+  publicly_accessible = true
+  skip_final_snapshot = true
+
+}
+
+resource "aws_security_group" "postgres_sg" {
+  name        = "postgres_sg"
+  description = "Allow inbound traffic on port 5432 from anywhere"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+output "rdb_endpoint" {
+  value     = aws_db_instance.rdb.endpoint
+  sensitive = true
+}
