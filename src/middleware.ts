7576b2e170a4154027f42532260046cb391cf061
jay-bhogayata <jaybhogayata53@gmail.com>
2024-06-18 18:36:05 +0530

    feat: add middleware to check is user is admin or not

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,3 +12,11 @@ export function isAuthenticated(
     res.status(401).json({ message: "Unauthorized" });
   }
 }
+
+export function isAdmin(req: Request, res: Response, next: NextFunction) {
+  if (req.session && (req.session as SessionData).user?.role === "admin") {
+    next();
+  } else {
+    res.status(401).json({ message: "Unauthorized" });
+  }
+}
