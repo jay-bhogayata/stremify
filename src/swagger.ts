fb0212d3b0782085707f2b9d9fbea4404da9bfc6
jay-bhogayata <jaybhogayata53@gmail.com>
2024-06-26 23:12:02 +0530

    feat: setup swagger docs using swagger-jsdoc and scalar

---
+++ src/swagger.ts
@@ -0,0 +1,33 @@
+import swaggerJsdoc from "swagger-jsdoc";
+
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Stremify API",
+      version: "0.0.1",
+      description: "A backend api for video streaming platform.",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
+      contact: {
+        name: "jay bhogayata",
+        email: "contact@jaybhogayata.dev",
+      },
+    },
+
+    components: {
+      securitySchemes: {
+        cookieAuth: {
+          type: "apiKey",
+          in: "cookie",
+          name: "sessionId",
+        },
+      },
+    },
+  },
+  apis: ["./src/routes/*.ts", "./src/controllers/*.ts", "./src/types.ts"],
+};
+
+export const swaggerDocs = swaggerJsdoc(swaggerOptions);
